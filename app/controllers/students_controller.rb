8798356df04a38e01b517744314617b50f1722ff
Sharnell Grant <sharnellgrant@gmail.com>
2020-06-21 16:02:10 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create()
+   @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
   end
 
 end
